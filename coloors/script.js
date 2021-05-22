a775973802d8545008c447bae2980b43c6abe92b
SaifN97 <saifnarpali@gmail.com>
2021-05-22 15:41:57 +0530

    Initial setup and random hex generator

---
+++ coloors/script.js
@@ -0,0 +1,36 @@
+// Global Selections
+const colorDivs = document.querySelectorAll(".color");
+const generateBtn = document.querySelector(".generate");
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHexes = document.querySelectorAll(".color h2");
+
+// Functions
+function generateHex() {
+  const hexColor = chroma.random();
+  return hexColor;
+}
+
+function randomColors() {
+  colorDivs.forEach((div, index) => {
+    const hexTxt = div.children[0];
+    const randomColor = generateHex();
+
+    // Adding the color to bg
+    div.style.backgroundColor = randomColor;
+    hexTxt.innerText = randomColor;
+
+    // Check for contrast
+    checkTextContrast(randomColor, hexTxt);
+  });
+}
+
+function checkTextContrast(color, text) {
+  const luminance = chroma(color).luminance();
+  if (luminance > 0.5) {
+    text.style.color = "black";
+  } else {
+    text.style.color = "white";
+  }
+}
+
+randomColors();
